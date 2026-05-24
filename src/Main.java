b5f1316a5cdd64e51e8cb89e8c1a274e088d209a
Tiu <yeskontakt9@mail.ru>
2026-05-24 21:58:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,19 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.println("\nHello");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        int[] array1={34,3,-43,23,64,1,4,-34,-4,21,54,1};
+        System.out.println("Not Sort");
+        Print.print(array1);
+        BubblesSort bubblesSort = new BubblesSort(array1);
+        bubblesSort.sort();
+        System.out.println("\nSort");
+        Print.print(array1);
     }
 }
