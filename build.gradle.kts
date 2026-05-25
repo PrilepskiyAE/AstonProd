207afbbf531677f8fedd0f5ebe863d3dd3b34fd7
Ilya Zernov <zzumber@gmail.com>
2026-05-25 18:37:36 +0300

    Add in Gradle UTF-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,3 +18,15 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<ProcessResources>().configureEach {
+    filteringCharset = "UTF-8"
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs("-Dfile.encoding=UTF-8", "-Dsun.stdout.encoding=UTF-8", "-Dsun.stderr.encoding=UTF-8")
+}
