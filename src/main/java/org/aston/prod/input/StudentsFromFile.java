60f0bbf2709105e8666233a76df77039bd6e9842
Ilya Zernov <zzumber@gmail.com>
2026-06-03 21:04:10 +0300

    Сигнатура для 3 участника.

---
+++ src/main/java/org/aston/prod/input/StudentsFromFile.java
@@ -0,0 +1,12 @@
+package org.aston.prod.input;
+
+import org.aston.prod.model.Student;
+
+import java.util.List;
+
+public class StudentsFromFile {
+    public static List<Student> readFile(String fileName, List<Student> students){
+
+        return null;
+    }
+}
