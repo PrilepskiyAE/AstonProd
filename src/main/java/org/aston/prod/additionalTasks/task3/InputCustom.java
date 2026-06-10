c2d60ef94ca7120b9311aab5f6a9d82e3c39c4df
Tiu <yeskontakt9@mail.ru>
2026-06-10 20:14:46 +0300

    дополнительное задание 3

--- src/main/java/org/aston/prod/additionalTasks/task3/InputCustom.java
+++ src/main/java/org/aston/prod/additionalTasks/task3/InputCustom.java
@@ -5,6 +5,7 @@ import org.aston.prod.model.Student;
 
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 
 import static org.aston.prod.additionalTasks.task3.CustomList.customCollector;
@@ -12,13 +13,22 @@ import static org.aston.prod.additionalTasks.task3.CustomList.customCollector;
 public class InputCustom {
 
     public static void consoleCustom(Scanner scanner) {
-        CustomList<Student> customStudent =  new CustomList<>();
-        customStudent.stream().map(i -> Student.builder().name("Dgfgf").age(45).group(43).build()).
-                collect(customCollector());
-        customStudent.stream().map(i -> Student.builder().name("Dgjjjjgf").age(76).group(23).build()).
-                forEach(System.out::println);
-        for (Student student : customStudent) {
-            System.out.println(student);
+        System.out.println("Сколько студентов введем?");
+        String inputSize = scanner.nextLine();
+        int size = Integer.parseInt(inputSize);
+        Student[] students = new Student[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Введите имя");
+            String name = scanner.nextLine();
+            System.out.println("Введите возраст");
+            String inputAge = scanner.nextLine();
+            int age = Integer.parseInt(inputAge);
+            System.out.println("Введите группу");
+            String inputGroup = scanner.nextLine();
+            int group = Integer.parseInt(inputGroup);
+            students[i] = Student.builder().name(name).age(age).group(group).build();
         }
+        CustomList<Student> customStudent = Stream.of(students).collect(customCollector());
+        System.out.println(customStudent);
     }
 }
